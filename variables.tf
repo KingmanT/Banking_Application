f590ae1553d132d1e07ffc6e619903bf09dde8c4
Kingman <107763929+KingmanT@users.noreply.github.com>
2023-06-12 11:23:07 -0400

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "profile_creds" {
+type = string
+default = "TrystanLove474_15"
+description = "Specifies the profile to be used"
+}
+variable "key_pair" {
+type = string
+default = "scriptKey"
+description = "Specifies the key pair to be used"
+}
+variable "public_subnet_1_cidr_region1" {
+  description = "CIDR Block for Public Subnet 1"
+  default     = "10.0.0.0/23"
+}
+variable "public_subnet_2_cidr_region1" {
+  description = "CIDR Block for Public Subnet 2"
+  default     = "10.0.2.0/23"
+}
+
+variable "public_subnet_1_cidr_region2" {
+  description = "CIDR Block for Public Subnet 1"
+  default     = "172.0.0.0/23"
+}
+variable "public_subnet_2_cidr_region2" {
+  description = "CIDR Block for Public Subnet 2"
+  default     = "172.0.2.0/23"
+}

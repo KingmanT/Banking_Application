7536a8ed571a2c39575efbc5dab76e6df8d50026
Kingman <107763929+KingmanT@users.noreply.github.com>
2023-06-13 12:03:17 -0400

    Update deploy.sh

--- Demo_Terraform/deploy.sh
+++ Demo_Terraform/deploy.sh
@@ -39,7 +39,7 @@
             }
         }
     }
-}' | jq . > config.json
+}' | jq . > /opt/aws/amazon-cloudwatch-agent/bin/config.json
   sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json
   sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a start
 

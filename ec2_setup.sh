056de7e16a0861db84289f320127a6f9ef58e3ff
Kingman <107763929+KingmanT@users.noreply.github.com>
2024-08-05 20:27:36 -0400

    Update ec2_setup.sh

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y openjdk-11-jre
+sudo apt update && sudo apt install -y fontconfig openjdk-17-jre
 
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian binary/ | sudo tee \
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
 
-sudo apt update
-sudo apt install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl status jenkins >> ~/Status.txt
 
